e92dc0de0d802637acb3aa441ecef78a26bc5d2f
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2020-10-22 18:44:41 -0400

    HTML updated, SCSS updated (Divs ordered, bgs added and colors set)

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const $imgAuthor = document.querySelector('.content__author__img'),
+    $imgBg = document.querySelector('.img');
+
+$imgAuthor.style.height = window.getComputedStyle($imgAuthor).width
+$imgBg.style.height = window.getComputedStyle($imgBg).width
