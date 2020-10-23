2ad8f943f6dd6d8a7e0dbf8d3cbff7ee5359379a
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2020-10-23 19:23:54 -0400

    SCSS updated, initial margins added, initial hover added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 const $imgAuthor = document.querySelector('.content__author__img'),
     $imgBg = document.querySelector('.img');
 
-$imgAuthor.style.height = window.getComputedStyle($imgAuthor).width
-$imgBg.style.height = window.getComputedStyle($imgBg).width
+$imgAuthor.style.height = window.getComputedStyle($imgAuthor).width;
+$imgBg.style.height = window.getComputedStyle($imgBg).width;
+
+window.addEventListener('resize', () => {
+    $imgAuthor.style.height = window.getComputedStyle($imgAuthor).width;
+    $imgBg.style.height = window.getComputedStyle($imgBg).width;
+});
