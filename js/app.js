900e795afc13e67505ae87dfe7aa56c8556bb216
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2020-10-26 16:34:06 -0400

    Margins added, bugs fixed, HTML updated, SCSS updated

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
-const $imgAuthor = document.querySelector('.content__author__img'),
-    $imgBg = document.querySelector('.img');
+const $imgBg = document.querySelector('.img');
 
-$imgAuthor.style.height = window.getComputedStyle($imgAuthor).width;
 $imgBg.style.height = window.getComputedStyle($imgBg).width;
 
 window.addEventListener('resize', () => {
-    $imgAuthor.style.height = window.getComputedStyle($imgAuthor).width;
     $imgBg.style.height = window.getComputedStyle($imgBg).width;
 });
