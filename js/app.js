5d6df04b2fccca82ac08bce357e9d74e08165175
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2020-11-04 00:25:30 -0400

    Project finished

--- js/app.js
+++ js/app.js
@@ -1,59 +1,28 @@
-const $imgBg = document.querySelector('.img'),
-        $mainHeight = document.querySelector('.main');
-
-const $author = document.querySelector('.content__author');
-
-const $img = document.querySelector('.content__author__img'),
-        $nameDate = document.querySelector('.content__author__name-date');
-
 const $shareBtn = document.querySelector('.content__author__share__img'),
-        $share = document.querySelector('.content__author__share'),
         $shareActive = document.querySelector('.content__author__share-active'),
-        $shareTriangle = document.querySelector('.content__author__share-active__triangle'),
-        $shareImgBtn = document.querySelector('.content__author__share__img'),
-        $shareImg = document.querySelector('.content__author__share__img-share');
+        $shareImg = document.querySelector('.content__author__share__img-share'),
+        $author = document.querySelector('.content__author'),
+        $authorImg = document.querySelector('.content__author__img'),
+        $date = document.querySelector('.content__author__name-date');
 
-function media() {
-    if (matchMedia("(max-width: 1023px)")) {
-        if ($shareActive.classList.contains('none')) {
-            $shareActive.classList.remove('none');
-            $shareActive.classList.add('share-active-media');
-            $shareImgBtn.classList.add('img-media');
-            $share.classList.add('share-media');
-            $author.classList.add('author-media');
-            $shareTriangle.style.display = 'none';
-            $img.style.display = 'none';
-            $nameDate.style.display = 'none';
-        } else{
-            $shareActive.classList.add('none');
-            $shareActive.classList.remove('share-active-media');
-            $shareImgBtn.classList.remove('img-media');
-            $share.classList.add('share-media');
-            $author.classList.remove('author-media');
-            $shareTriangle.style.display = 'block';
-            $img.style.display = 'flex';
-            $nameDate.style.display = 'flex';
-        }
+function btnClicked() {
+    
+    if ($shareActive.classList.contains('none')) {
+        $shareActive.classList.remove('none');
+        $shareBtn.classList.add('active');
+        $authorImg.classList.add('none');
+        $date.classList.add('none');
+        $author.style.margin = 0;
     } else {
-        if ($shareActive.classList.contains('none')) {
-            $shareActive.classList.remove('none');
-        } else{
-            $share.classList.add('none');
-        }
+        $shareActive.classList.add('none');
+        $shareBtn.classList.remove('active');
+        $authorImg.classList.remove('none');
+        $date.classList.remove('none');
+        $author.style.margin = '0 2.5rem 1rem 2.5rem';
     }
-
-
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    $imgBg.style.height = getComputedStyle($mainHeight).height;
-});
-
-window.addEventListener('resize', () => {
-    $imgBg.style.height = getComputedStyle($mainHeight).height;
-});
-
-$shareBtn.addEventListener('click', media);
+$shareBtn.addEventListener('click', btnClicked);
 
 $shareBtn.addEventListener('mouseover', () => {
     $shareImg.src = "./images/icon-share-hover.svg";
